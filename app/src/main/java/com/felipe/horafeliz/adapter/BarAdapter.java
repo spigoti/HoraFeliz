16f3017a9bc413cd94c73cdeac08f1409f8b7e88
Augusto dos Santos Oliveira <6200559@univali.br>
2019-12-04 21:49:40 -0300

    Implementei Bar Adapter, comecei BarViewHolder

--- app/src/main/java/com/felipe/horafeliz/adapter/BarAdapter.java
+++ app/src/main/java/com/felipe/horafeliz/adapter/BarAdapter.java
@@ -1,4 +1,38 @@
 package com.felipe.horafeliz.adapter;
 
-public class BarAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.felipe.horafeliz.R;
+import com.felipe.horafeliz.model.Bar;
+import com.felipe.horafeliz.model.BarDao;
+
+public class BarAdapter extends RecyclerView.Adapter{
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ConstraintLayout elementoBarXML = (ConstraintLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_local,parent,false);
+
+        BarViewHolder gaveta =  new BarViewHolder(elementoBarXML);
+
+        return gaveta;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Bar bar= BarDao.obterInstancia().obterBar().get(position);
+        BarViewHolder gaveta = (BarViewHolder) holder;
+        gaveta.atualizaGavetaComOBarQueChegou(bar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return BarDao.obterInstancia().obterBar().size();
+    }
 }
--- app/src/main/java/com/felipe/horafeliz/adapter/BarViewHolder.java
+++ app/src/main/java/com/felipe/horafeliz/adapter/BarViewHolder.java
@@ -1,4 +1,24 @@
 package com.felipe.horafeliz.adapter;
 
-public class BarViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.felipe.horafeliz.model.Bar;
+
+public class BarViewHolder extends RecyclerView.ViewHolder {
+    private ConstraintLayout clPai;
+    private String idBar;
+    private ImageView img;
+
+    public BarViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+
+    public void atualizaGavetaComOBarQueChegou(Bar bar) {
+    }
 }
